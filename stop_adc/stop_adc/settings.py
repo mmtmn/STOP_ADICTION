4d3c7660e7bc29ca6b0fa1f82e037f930137352d
thiago <thiagomnobrega@gmail.com>
2020-03-15 22:08:40 -0300

    added mysql db

--- stop_adc/stop_adc/settings.py
+++ stop_adc/stop_adc/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'stop_adc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'StopAdcDB',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
